289649b442b7ced73de236fba6f46f8197051e81
Martín <martin.d.pintos@gmail.com>
2024-01-20 23:08:33 -0300

    feat: add tab screens

--- app/(tabs)/blog/index.tsx
+++ app/(tabs)/blog/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "@/components/Themed";
 
-export default function HomeScreen() {
+export default function BlogScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">Home</Text>
+      <Text className="text-xl font-bold">Blog</Text>
     </View>
   );
 }
--- app/(tabs)/projects/index.tsx
+++ app/(tabs)/projects/index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "@/components/Themed";
 
-export default function HomeScreen() {
+export default function ProjectsScreen() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">Home</Text>
+      <Text className="text-xl font-bold">Projects</Text>
     </View>
   );
 }
